1b99c93bb87c882eacbc58018fd297552f50df09
PBDm <pbdm915@gmail.com>
2018-05-17 23:27:27 +0800

    Revert "use http for bootcss cdn"
    This reverts commit a38ae3546e605a636ae74aa1aa3c5f829f60fd5d.

--- js/config.js
+++ js/config.js
@@ -4,10 +4,10 @@ export const SQUENCE_PATH = [
   '//cdn.bootcss.com/js-sequence-diagrams/1.0.6/sequence-diagram-min.js'
 ];
 export const MERMAID_PATH = [
-  'http://cdn.bootcss.com/mermaid/7.0.0/mermaid.min.js',
-  'http://cdn.bootcss.com/mermaid/7.0.0/mermaid.forest.min.css'
+  '//cdn.bootcss.com/mermaid/7.0.0/mermaid.min.js',
+  '//cdn.bootcss.com/mermaid/7.0.0/mermaid.forest.min.css'
 ];
 
 export const FLOWCHART_PATH = [
-  'http://cdn.bootcss.com/flowchart/1.7.0/flowchart.min.js'
+  '//cdn.bootcss.com/flowchart/1.7.0/flowchart.min.js'
 ];
