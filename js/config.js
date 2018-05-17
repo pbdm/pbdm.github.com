b119e894176a89579be38e637bcd4bbcc5c6feea
PBDm <pbdm915@gmail.com>
2018-05-17 23:19:36 +0800

    use http for bootcss cdn

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 export const SQUENCE_PATH = [
-  '//cdn.bootcss.com/underscore.js/1.8.3/underscore-min.js',
-  '//cdn.bootcss.com/raphael/2.2.1/raphael.min.js',
-  '//cdn.bootcss.com/js-sequence-diagrams/1.0.6/sequence-diagram-min.js'
+  'http://cdn.bootcss.com/underscore.js/1.8.3/underscore-min.js',
+  'http://cdn.bootcss.com/raphael/2.2.1/raphael.min.js',
+  'http://cdn.bootcss.com/js-sequence-diagrams/1.0.6/sequence-diagram-min.js'
 ];
 export const MERMAID_PATH = [
   '//cdn.bootcss.com/mermaid/7.0.0/mermaid.min.js',
