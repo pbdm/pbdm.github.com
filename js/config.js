7ab1180e2439982c35d0323453c8a17c1a2cfa4c
PBDm <pbdm915@gmail.com>
2018-05-23 10:43:39 +0800

    update cdn

--- js/config.js
+++ js/config.js
@@ -1,13 +1,13 @@
 export const SQUENCE_PATH = [
-  '//cdn.bootcss.com/underscore.js/1.8.3/underscore-min.js',
-  '//cdn.bootcss.com/raphael/2.2.1/raphael.min.js',
-  '//cdn.bootcss.com/js-sequence-diagrams/1.0.6/sequence-diagram-min.js'
+  '//cdn.jsdelivr.net/npm/underscore@1.9.0/underscore.min.js',
+  '//cdn.jsdelivr.net/npm/raphael@2.2.7/raphael.min.js',
+  '//cdn.jsdelivr.net/npm/js-sequence-diagrams@1000000.0.6/fucknpm/sequence-diagram-min.min.jss'
 ];
 export const MERMAID_PATH = [
-  '//cdn.bootcss.com/mermaid/7.0.0/mermaid.min.js',
+  '//cdn.jsdelivr.net/npm/mermaid@8.0.0-rc.8/dist/mermaid.min.js',
   '//cdn.bootcss.com/mermaid/7.0.0/mermaid.forest.min.css'
 ];
 
 export const FLOWCHART_PATH = [
-  '//cdn.bootcss.com/flowchart/1.7.0/flowchart.min.js'
+  '//cdn.jsdelivr.net/npm/flowchart.js@1.11.0/release/flowchart.min.jss'
 ];
